748030fb7abedf6f1968c9d2a10c6b67a8c0e831
Robert De La Cruz <rdlc976@gmail.com>
2023-04-25 17:11:15 -0400

    up to routes and views

--- app.js
+++ app.js
@@ -42,4 +42,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+  
 module.exports = app;
--- seeds.js
+++ seeds.js
@@ -1,9 +1,18 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 
-const User = require('../models/User.model');
+
+const User = require('./models/User.model');
 
 // ℹ️ Connects to the database
-require("../db");
+mongoose.connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 // User.collection.drop();
 
